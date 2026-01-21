4f2a5218f5ee052d47328d36d9f41692a686b4ea
Yousaf <yousaf.khan@frankvaliant.com>
2026-01-21 13:49:41 +0100

    Can push and pull from git before rendering Version: 0.5.21+dev.2

--- client/ayon_deadline/plugins/publish/unreal/submit_unreal_deadline.py
+++ client/ayon_deadline/plugins/publish/unreal/submit_unreal_deadline.py
@@ -1,5 +1,6 @@
 import os
 import ayon_api
+import subprocess
 from dataclasses import dataclass, field, asdict
 import pyblish.api
 from datetime import datetime
@@ -76,8 +77,73 @@ class UnrealSubmitDeadline(
         
         project = os.environ.get("AYON_PROJECT_NAME")
         project_entity = ayon_api.get(f"projects/{project}")
+        local_project_root = project_entity["config"]["roots"]["work_unreal"]["windows"]
         project_root = project_entity["config"]["roots"]["work_unreal_server"]["windows"]
         render_path = project_entity["config"]["roots"]["renders"]["windows"]
+        
+        # --- Git Variables ---------------------------------------------------------
+        ORG = "frankvaliant"
+        PAT = "4nHIQJLb4PDyxshKT5964V25qKiaudYeRVOnDol4UeIsraqi3zpRJQQJ99CAACAAAAAT9cK1AAASAZDO34sG"
+        GIT_BASH = r"C:\Program Files\Git\bin\bash.exe"
+
+        auto_commit_message = "Auto commit before Rendering"
+        server_project_path = os.path.dirname(project_root)
+        project_name = os.path.basename(server_project_path)
+        local_project_path = f"{local_project_root}/{project_name}"
+        self.log.debug(f">>> Local path: {local_project_path}")
+        self.log.debug(f">>> Server path: {server_project_path}")
+        self.log.debug(f">>> Project Name: {project_name}")
+
+        repo_url = f"https://{PAT}@dev.azure.com/{ORG}/{project_name}/_git/{project_name}"
+
+        # --- Git Commit and Push ---------------------------------------------------
+        subprocess.check_call(["git", "-C", local_project_path, "remote", "set-url", "origin", repo_url])
+        subprocess.check_call(["git", "-C", local_project_path, "add", "."])
+        
+        try:
+            subprocess.check_call(["git", "-C", local_project_path, "commit", "-m", auto_commit_message])
+        except subprocess.CalledProcessError as e:
+            self.log.debug("Nothing to commit or an error occurred:", e)
+
+        
+        push_result = subprocess.run(
+            ["git", "-C", local_project_path, "push", "-u", "origin", "master"],
+            capture_output=True,
+            text=True,
+            check=False
+        )
+
+        self.log.debug(f"stdout: {push_result.stdout}")
+        self.log.debug(f"stderr: {push_result.stderr}")
+        self.log.debug(f"returncode: {push_result.returncode}")
+
+        # --- Pull from git on Server -----------------------------------------------
+        if not os.path.exists(server_project_path):
+            self.log.debug(f">>> Cloning repository to: {server_project_path}")
+            subprocess.check_call(["git", "clone", repo_url, server_project_path])
+        else:           
+            self.log.debug(f">>> Pulling latest changes to: {server_project_path}")
+            
+            subprocess.run(
+                ["git", "-C", server_project_path, "fetch", "origin"],
+                check=True
+            )
+
+            subprocess.run(
+                ["git", "-C", server_project_path, "checkout", "master"],
+                check=True
+            )
+
+            pull_result = subprocess.run(
+                ["git", "-C", server_project_path, "reset", "--hard", "origin/master"],
+                capture_output=True,
+                text=True,
+                check=True
+            )
+            
+            self.log.debug(f"stdout: {pull_result.stdout}")
+            self.log.debug(f"stderr: {pull_result.stderr}")
+            self.log.debug(f"returncode: {pull_result.returncode}")
 
         #render_path = self._instance.data["expectedFiles"][0]
         #self._instance.data["outputDir"] = os.path.dirname(render_path)
--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "deadline"
 title = "Deadline"
-version = "0.5.21+dev"
+version = "0.5.21+dev.2"
 
 client_dir = "ayon_deadline"
 
